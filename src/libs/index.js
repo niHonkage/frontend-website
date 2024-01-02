5b3fc2df36b5094146ce80ccd6040077a2ba241a
Rikki <wanava89@gmail.com>
2024-01-02 23:52:27 +0800

    修正循环错误

--- src/libs/index.js
+++ src/libs/index.js
@@ -4,7 +4,7 @@ export default {
     // 获取特定路径下的所有index.vue文件
     const components = import.meta.glob('./*/index.vue')
     // 循环 路径：导入语句 的键值对
-    for (const [key, value] of components) {
+    for (const [key, value] of Object.entries(components)) {
       // 拼接组件名
       const componentName = key.replace('./', '').split('/')[0]
       app.component(componentName, defineAsyncComponent(value))

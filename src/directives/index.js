b59e58e3a849c6a7a60900f7f99cd784bb84c2a1
Rikki <wanava89@gmail.com>
2024-01-08 01:03:06 +0800

    组件自动注册

--- src/directives/index.js
+++ src/directives/index.js
@@ -2,6 +2,14 @@ import lazy from './modules/lazy.js'
 
 export default {
   install(app) {
-    app.directive('lazy', lazy)
+    // import.meta.globEager 为同步导入
+    const directives = import.meta.globEager('./modules/*.js')
+    const directiveArr = Object.entries(directives)
+    for (const [key, value] of directiveArr) {
+      // 分割路径名选择组件name
+      const strArr = key.split('/')
+      const directiveName = strArr[strArr.length - 1].replace('.js', '')
+      app.directive(directiveName, value.default)
+    }
   }
 }
